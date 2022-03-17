780755e2497720ddbe723389fc4f5cea976d3caa
HyeokChan <qpjiq123@gmail.com>
2022-03-17 23:49:48 +0900

    준영속 상태

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -16,10 +15,12 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Member member = new Member(200L, "member200");
-            em.persist(member);
-            em.flush();
-            System.out.println("===============");
+            Member member = em.find(Member.class, 200L);
+            member.setName("123");
+
+            em.clear();
+
+            Member member2 = em.find(Member.class, 200L);
 
             //쿼리 발생
             tx.commit();
