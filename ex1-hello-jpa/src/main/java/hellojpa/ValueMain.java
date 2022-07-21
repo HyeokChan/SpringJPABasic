0221bd36db20db07ed5d6ba7389a317a5a211094
kwonhyeokchan <qpjiq123@gmail.com>
2022-07-22 00:38:44 +0900

    값 타입의 비교

--- ex1-hello-jpa/src/main/java/hellojpa/ValueMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/ValueMain.java
@@ -2,7 +2,11 @@ package hellojpa;
 
 public class ValueMain {
     public static void main(String[] args) {
+        Address address1 = new Address("city", "stt", "11233");
+        Address address2 = new Address("city", "stt", "11233");
 
+        // 오버라이드한 equals
+        System.out.println("address1 equal address2 " + address1.equals(address2));
 
     }
 }
