38140344459912fc9f7756c3db081539632c4d45
kwonhyeokchan <qpjiq123@gmail.com>
2022-08-18 00:17:20 +0900

    JPQL 타입 표현과 기타식

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -19,17 +19,23 @@ public class JpaMain {
             member.setUsername("teamA");
             member.setAge(10);
             member.changeTeam(team);
+            member.setType(MemberType.ADMIN);
             em.persist(member);
 
             em.flush();
             em.clear();
 //            String query = "select m from Member m left outer join m.team t on t.name = 'teamA'";
 //            String query = "select m from Member m left outer join Team t on m.username = t.name and t.name = 'teamA'";
-            String query = "select (select avg(m1.age) from Member m1) as avgAge, m from Member m";
-            List<Member> resultList = em.createQuery(query, Member.class)
+//            String query = "select (select avg(m1.age) from Member m1) as avgAge, m from Member m";
+            String query = "select m.username, 'HELLO', true, m.type from Member m " +
+                            "where m.type = :userType";
+            List<Object[]> resultList = em.createQuery(query)
+                    .setParameter("userType", MemberType.ADMIN)
                     .getResultList();
-            for (Member member1 : resultList) {
-                System.out.println("member1 = " + member1);
+            for(Object[] objects : resultList){
+                for(Object o : objects){
+                    System.out.println("objects = " + o);
+                }
             }
             tx.commit();
         } catch (Exception e) {
